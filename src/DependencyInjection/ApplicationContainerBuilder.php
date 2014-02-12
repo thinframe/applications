933ae1356d5c65ec364fe79b49d78056e78abed1
Sorin <sorin.badea91@gmail.com>
2014-02-13 00:07:28 +0200

    Cleaning artifacts

--- src/DependencyInjection/ApplicationContainerBuilder.php
+++ src/DependencyInjection/ApplicationContainerBuilder.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * src/DependencyInjection/ApplicationContainerBuilder.php
+ *
+ * @author    Sorin Badea <sorin.badea91@gmail.com>
+ * @license   MIT license (see the license file in the root directory)
+ */
+
 namespace ThinFrame\Applications\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
